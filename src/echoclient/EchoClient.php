1a09f93e18c567141f96f9f006a96c1541bcea29
chris <chris.clarke@talis.com>
2014-05-19 14:42:46 +0100

    Adding http_build_query

--- src/echoclient/EchoClient.php
+++ src/echoclient/EchoClient.php
@@ -188,12 +188,8 @@ class EchoClient
         }
 
         $eventUrl = $baseUrl.'/analytics/'.$type.'?class='.urlencode($class);
-        foreach($opts as $key=>$value)
-        {
-            if (!empty($value))
-            {
-                $eventUrl .= '&'.$key.'='.urlencode($value);
-            }
+        if (count($opts)>0) {
+            $eventUrl .= http_build_query($opts);
         }
 
         $client = $this->getHttpClient();
